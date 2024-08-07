d21c4bad5fbf78e551fc6a181bebbefe53f6acf4
namoongili <ansrlf0124@naver.com>
2024-08-07 17:49:23 +0900

    Add files via upload

---
+++ day05/고객정보출력.java
@@ -0,0 +1,19 @@
+package day05;
+
+public class 고객정보출력 {
+
+    public static void main(String[] args) {
+    	// 고객 정보 변수 선언
+    	String id = "A001";
+    	String name = "홍길동";
+    	String age = "30";
+    	String height = "175";
+    	
+    	// 고객 정보 출력
+    	System.out.println("아이디: " + id);
+    	System.out.println("이름: " + name);
+    	System.out.println("나이: " + age);
+    	System.out.println("키: " + height);
+    }
+}
+
---
+++ day05/합구하기.java
@@ -0,0 +1,10 @@
+package day05;
+
+public class 합구하기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
